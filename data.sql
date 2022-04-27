d99ddd99efa417c329f899ab10f76e99cb67a145
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-04-27 15:53:01 +0300

    Fix issues based on the feedback

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Agumon','03/02/2020',0,TRUE,10.23)
-INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Gabumon','15/11/2018',2,TRUE,8.0)
-INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Pikachu','07/01/2021',1,FALSE,15.04)
-INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Devimon','12/05/2017',5,TRUE,11.0)
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Agumon','2020/02/03',0,TRUE,10.23);
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Gabumon','2018/11/15',2,TRUE,8.0);
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Pikachu','2021/01/07',1,FALSE,15.04);
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Devimon','2017/05/12',5,TRUE,11.0);
 

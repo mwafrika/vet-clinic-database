35556ac903c9af54d9db71ea20d5f71733ce32d6
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-04-28 11:52:52 +0300

    Add foreign keys

--- schema.sql
+++ schema.sql
@@ -4,3 +4,21 @@ CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,name VARCHA
 BOOL NOT NULL,weight_kg DECIMAL(50,2) NOT NULL);
 
 ALTER TABLE animals ADD species VARCHAR(50);
+
+-- Create a table named owners with the following columns:
+CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,full_name VARCHAR NOT NULL,age INT NOT NULL);
+
+-- Remove column species
+ALTER TABLE animals DROP species;
+
+-- add species_id column to animals table
+ALTER TABLE animals ADD species_id INT;
+
+-- Add a foreign key constraint to the species_id column
+ALTER TABLE animals ADD FOREIGN KEY(species_id) REFERENCES species(id);
+
+-- add owner_id column to animals table
+ALTER TABLE animals ADD owner_id INT;
+
+-- Add a foreign key constraint to the owner_id column
+ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
